56cd8a636d9e42891ca40cb589f7f5eb0360ebac
ritchie46 <ritchie46@gmail.com>
2020-03-04 16:52:04 +0100

    refactor to library

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,7 @@
 pub mod mandelbrot;
 pub use mandelbrot::{parse_complex, parse_pair, render_concurrently, write_image};
+use wasm_bindgen::prelude::*;
+
+// Use `wee_alloc` as the global allocator.
+#[global_allocator]
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
