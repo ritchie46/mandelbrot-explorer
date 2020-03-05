c0695f09b4e5c947eefe5d55a9490e0d57187736
ritchie46 <ritchie46@gmail.com>
2020-03-05 16:46:00 +0100

    arrow input for pan and ctrl+arrow for zoom

--- Makefile
+++ Makefile
@@ -2,6 +2,9 @@
 run:
 	@cd www && npm run start
 
+build-release:
+	@wasm-pack build --release
 
 build:
 	@wasm-pack build
+
--- www/index.js
+++ www/index.js
@@ -1,14 +1,23 @@
 import {Mandelbrot, init_panic_hook} from "mandelbrot";
 import {memory} from "mandelbrot/mandelbrot_bg";
 
-const WIDTH = 200;
-const HEIGHT = 200;
+const WIDTH = 400;
+const HEIGHT = 400;
+const panStep = 0.01;
+const zoomStep = 0.01;
+let upper_left_re = -1;
+let upper_left_im = 0.2;
+let lower_right_re = -1.2;
+let lower_right_im = 0.35;
+
 
 init_panic_hook();
-const mb = Mandelbrot.new(WIDTH, HEIGHT, -1.2, 0.35, -1, 0.2);
+const mb = Mandelbrot.new(WIDTH, HEIGHT, upper_left_re, upper_left_im, lower_right_re, lower_right_im);
 mb.render();
 
 const canvas = document.querySelector("canvas");
+canvas.width = WIDTH;
+canvas.height = HEIGHT;
 const ctx = canvas.getContext("2d");
 const canvasImageData = ctx.createImageData(canvas.width, canvas.height);
 
@@ -24,9 +33,52 @@ const drawCanvas = () => {
             const idx = row * HEIGHT + col;
             const pixelVal = pixels[idx];
             ctx.fillStyle = `rgb(${pixelVal}, ${pixelVal}, ${pixelVal})`;
-            ctx.fillRect(row, col, 1, 1)
+            ctx.fillRect(col, row, 1, 4)
         }
     }
 };
 
 drawCanvas();
+
+document.addEventListener("keydown", function (e) {
+    e.preventDefault();
+    const key = e.key;
+
+    if (e.ctrlKey) {
+        switch (key) {
+            case "ArrowDown":
+                upper_left_re += zoomStep;
+                lower_right_im += zoomStep;
+                console.log(upper_left_re, upper_left_im, lower_right_re, lower_right_im);
+                break;
+            case "ArrowUp":
+                upper_left_re -= zoomStep;
+                lower_right_im -= zoomStep;
+                break;
+        }
+    } else {
+        switch (key) {
+            case "ArrowRight":
+                upper_left_re -= panStep;
+                lower_right_re -= panStep;
+                break;
+            case "ArrowLeft":
+                upper_left_re += panStep;
+                lower_right_re += panStep;
+                break;
+            case "ArrowDown":
+                upper_left_im += panStep;
+                lower_right_im += panStep;
+                break;
+            case "ArrowUp":
+                upper_left_im -= panStep;
+                lower_right_im -= panStep;
+                break;
+        }
+    }
+
+
+    mb.zoom(upper_left_re, upper_left_im, lower_right_re, lower_right_im);
+    mb.render();
+    drawCanvas()
+});
